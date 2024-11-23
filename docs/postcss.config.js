0d5f2444a05c08439a573b6f2606b9a0a1b0f378
David Sillman <dsillman2000@gmail.com>
2024-11-22 20:59:35 -0500

    configuring workflow to build site twice to properly include all tailwind css classes

--- docs/postcss.config.js
+++ docs/postcss.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-    plugins: [
-      require('tailwindcss'),
-      require('autoprefixer'),
-      ...(process.env.JEKYLL_ENV == 'production'
-        ? [require('cssnano')({ preset: 'default' })]
-        : [])
-    ]
-  }
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('postcss-import')({
+      addModulesDirectories: ["assets/css"]
+    }),
+    ...(process.env.JEKYLL_ENV == 'production'
+      ? [require('cssnano')({ preset: 'default' })]
+      : [])
+  ]
+}

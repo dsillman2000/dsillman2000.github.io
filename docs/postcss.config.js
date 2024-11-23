27ee13638d5b748382dc6f9ada12ae008e3d256c
David Sillman <dsillman2000@gmail.com>
2024-11-23 12:15:29 -0500

    still trying to fix tailwind custom css classes in deployment

--- docs/postcss.config.js
+++ docs/postcss.config.js
@@ -3,7 +3,7 @@ module.exports = {
     require('tailwindcss'),
     require('autoprefixer'),
     require('postcss-import')({
-      addModulesDirectories: ["assets/css"]
+      addModulesDirectories: ["assets/css", "_site/assets/css"]
     }),
     ...(process.env.JEKYLL_ENV == 'production'
       ? [require('cssnano')({ preset: 'default' })]
